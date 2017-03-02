1af8fe212d377747b2a353c28830114b44ea2674
Adam O'Hern <adam@mechanicalcolor.com>
2017-03-02 12:07:54 -0500

    fake data for an actual macro

--- lxserv/replay_fakeData.py
+++ lxserv/replay_fakeData.py
@@ -10,11 +10,22 @@ class CommandClass(replay_commander.CommanderClass):
     def commander_execute(self, msg, flags):
         jack = replay.ReplayLumberjack()
 
-        task = jack.add_child()
-        task.values['name'].value = "Render"
-        task.values['command'].value = 'render'
-        task.values['enable'].value = True
+        commands = [
+            ('Primitive', 'script.run "macro.scriptservice:27554333777:macro"'),
+            ('Transform Channel', 'transform.channel pos.X 1.0'),
+            ('Primitive', 'script.run "macro.scriptservice:19601433555:macro"'),
+            ('Transform Channel', 'transform.channel pos.X -1.0'),
+            ('Primitive', 'script.run "macro.scriptservice:47158833888:macro"'),
+            ('Transform Channel', 'transform.channel pos.Y 1.0')
+        ]
 
-
+        for command in commands:
+            task = jack.add_child()
+            task.values['name'].value = command[0]
+            task.values['command'].value = command[1]
+            task.values['command'].color.special = 4113
+            task.values['enable'].value = True
+            task.values['enable'].cell_command = 'select.typeFrom "item;pivot;center;edge;polygon;vertex;ptag" ?'
+            task.values['enable'].batch_command = 'select.typeFrom "item;pivot;center;edge;polygon;vertex;ptag" ?'
 
 lx.bless(CommandClass, 'replay.fakeData')
