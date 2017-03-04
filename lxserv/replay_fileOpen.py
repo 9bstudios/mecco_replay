e6ae442066d62587c8c4b056779a7c119219e660
adamohern <adam@mechanicalcolor.com>
2017-03-04 09:40:54 -0500

    fileOpen accepts a path

--- lxserv/replay_fileOpen.py
+++ lxserv/replay_fileOpen.py
@@ -8,11 +8,23 @@ class CommandClass(replay.commander.CommanderClass):
     """Reads a file from disk and parses it into the `Macro()` object's built-in
     parse methods."""
 
+    def commander_arguments(self):
+        return [
+            {
+                'name': 'path',
+                'datatype': 'string',
+                'flags': ['optional']
+            }
+        ]
+
     def commander_execute(self, msg, flags):
 
-        # Get the complete file path from the user:
-        input_path = modo.dialogs.customFile(dtype = 'fileOpen', title = 'Open LXM file', \
-           names = ('LXM',), unames = ('LXM file'), patterns = ('*.LXM', '*.lxm'))
+        input_path = self.commander_arg_value(0)
+
+        if not input_path:
+            # Get the complete file path from the user:
+            input_path = modo.dialogs.customFile(dtype = 'fileOpen', title = 'Open LXM file', \
+               names = ('LXM',), unames = ('LXM file'), patterns = ('*.LXM', '*.lxm'))
 
         replay.Macro().parse_LXM(input_path)
 
