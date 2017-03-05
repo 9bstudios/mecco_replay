d71e48c18f2073cfc216d1584617de252db5d169
adamohern <adam@mechanicalcolor.com>
2017-03-05 14:07:05 -0500

    require parent and arg_index for MacroCommandArg init

--- replay/MacroCommand.py
+++ replay/MacroCommand.py
@@ -239,9 +239,7 @@ class MacroCommand(lumberjack.TreeNode):
 
         # Populate the list.
         for n in range(len(argNames)):
-            cmd = MacroCommandArg(parent=self)
-            self.args.append(cmd)
-            cmd.init()
+            self.args.append(MacroCommandArg(parent=self, arg_index=n))
 
 
     def command_meta(self):
