aeba344115b68030b644c5501c8436c0c5e10a60
adamohern <adam@mechanicalcolor.com>
2017-03-06 14:22:24 -0500

    working on record toggle

--- lxserv/replay_record.py
+++ lxserv/replay_record.py
@@ -12,10 +12,19 @@ class RecordCommandClass(replay.commander.CommanderClass):
     inside the kit directory, then read and parsed using the `Macro().parse_LXM()`
     method. The user will then be prompted to save the file using `replay.fileSave`."""
 
+    _recording = False
+
     def commander_arguments(self):
         return [
             {
                 'name': 'mode',
+                'datatype': 'string',
+                'default': 'toggle',
+                'values_list_type': 'popup',
+                'values_list': ['toggle', 'start', 'stop'],
+                'flags': ['optional']
+            }, {
+                'name': 'status',
                 'datatype': 'boolean',
                 'default': True,
                 'flags': ['query', 'optional']
@@ -23,42 +32,45 @@ class RecordCommandClass(replay.commander.CommanderClass):
         ]
 
     def commander_execute(self, msg=None, flags=None):
-        mode = self.commander_arg_value(0, True)
+        mode = self.commander_arg_value(0, 'toggle')
+        state = self.__class__._recording
 
-        if mode:
-            # In case it's already recording, start over.
-            lx.eval('macro.record false')
-
-            # Start recording.
-            lx.eval('macro.record true')
-
-        elif not mode:
-            try:
-                # Store the temp file in the root kit directory.
-                temp_file_path = lx.eval('query platformservice alias ? "kit_mecco_replay:tmp.LXM"')
-
-                # Try saving the file.
-                lx.eval('macro.saveRecorded {%s}' % temp_file_path)
-
-                # Open the saved macro for editing.
-                lx.eval('replay.fileOpen {%s}' % temp_file_path)
-
-            except:
-                # The Macro recording was probably empty.
-                pass
+        lx.out(mode)
+        if mode == 'toggle':
+            state = False if state else True
+        elif mode == 'start':
+            state = True
+        elif mode == 'stop':
+            state = False
+        #
+        # if mode:
+        #     # In case it's already recording, start over.
+        #     lx.eval('macro.record false')
+        #
+        #     # Start recording.
+        #     lx.eval('macro.record true')
+        #
+        # elif not mode:
+        #     try:
+        #         # Store the temp file in the root kit directory.
+        #         temp_file_path = lx.eval('query platformservice alias ? "kit_mecco_replay:tmp.LXM"')
+        #
+        #         # Try saving the file.
+        #         lx.eval('macro.saveRecorded {%s}' % temp_file_path)
+        #
+        #         # Open the saved macro for editing.
+        #         lx.eval('replay.fileOpen {%s}' % temp_file_path)
+        #
+        #     except:
+        #         # The Macro recording was probably empty.
+        #         pass
 
     def cmd_Query(self,index,vaQuery):
-        """Hack."""
-        mode = self.commander_arg_value(0, True)
+        # Fires whenever any argument is queried.
 
-        # Invert the value
-        mode = False if mode else True
+        va = lx.object.ValueArray()
+        va.set(vaQuery)
 
-        self.commander_execute()
-
-        # va = lx.object.ValueArray()
-        # va.set(vaQuery)
-        #
         # args = self.commander_arguments()
         #
         # if index < len(args):
@@ -68,6 +80,10 @@ class RecordCommandClass(replay.commander.CommanderClass):
         #
         #     if is_query and is_not_fcl and has_recent_value:
         #         va.AddString(has_recent_value)
+
+        lx.out('queried', int(self._recording))
+
+        va.AddInt(int(self._recording))
 
         return lx.result.OK
 
