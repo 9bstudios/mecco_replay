64bf1f11c06181a508af53702d9c289fcf20b715
adamohern <adam@mechanicalcolor.com>
2017-03-06 15:07:01 -0500

    added record button

---
+++ lxserv/replay_fileInsert.py
@@ -0,0 +1,38 @@
+import lx, modo, replay
+
+"""A simple example of a blessed MODO command using the commander module.
+https://github.com/adamohern/commander for details"""
+
+
+class CommandClass(replay.commander.CommanderClass):
+    """Reads a file from disk and parses it into the `Macro()` object's built-in
+    parse methods."""
+
+    def commander_arguments(self):
+        return [
+            {
+                'name': 'path',
+                'datatype': 'string',
+                'flags': ['optional']
+            }
+        ]
+
+    def commander_execute(self, msg, flags):
+
+        # Try to get the path from the command line:
+        input_path = self.commander_arg_value(0)
+
+        macro = replay.Macro()
+
+        # Get the path from the user, if not given as argument:
+        if not input_path:
+            input_path = modo.dialogs.customFile(dtype = 'fileOpen', title = 'Open LXM file', \
+               names = macro.format_names, unames = macro.format_unames, patterns = macro.format_patterns)
+            if input_path is None:
+                return
+
+        # Parse the file in replay.Macro() and rebuild the view:
+        macro.parse(input_path)
+        macro.rebuild_view()
+
+lx.bless(CommandClass, 'replay.fileInsert')
--- lxserv/replay_record.py
+++ lxserv/replay_record.py
@@ -6,15 +6,22 @@ https://github.com/adamohern/commander for details"""
 
 class RecordCommandClass(replay.commander.CommanderClass):
     """Start or stop Macro recording. The `mode` argument starts recording when
-    `True` (or empty), stops recording when `False`, and toggles recording when queried.
+    `start`, stops recording when `stop`, and toggles recording when `toggle`.
+
+    The `query` argument should be queried for toolbar highlighting.
 
     When recording stops, the resulting file will be saved to a temporary location
     inside the kit directory, then read and parsed using the `Macro().parse_LXM()`
-    method. The user will then be prompted to save the file using `replay.fileSave`."""
+    method and inserted after the current primary command."""
 
+    # Whether or not we are currently recording.
+    # TODO We currently track this manually, so enabling or disabling macro recording
+    # in any other way will break this command. We should find a listener.
     _recording = False
 
     def commander_arguments(self):
+        """Command takes two arguments: `mode` and `query`.
+        `mode` can be `start`, `stop`, or `toggle`."""
         return [
             {
                 'name': 'mode',
@@ -24,68 +31,63 @@ class RecordCommandClass(replay.commander.CommanderClass):
                 'values_list': ['toggle', 'start', 'stop'],
                 'flags': ['optional']
             }, {
-                'name': 'status',
+                'name': 'query',
                 'datatype': 'boolean',
                 'default': True,
                 'flags': ['query', 'optional']
             }
         ]
 
+    @classmethod
+    def set_state(cls, state):
+        cls._recording = state
+
     def commander_execute(self, msg=None, flags=None):
         mode = self.commander_arg_value(0, 'toggle')
-        state = self.__class__._recording
 
-        lx.out(mode)
-        if mode == 'toggle':
-            state = False if state else True
-        elif mode == 'start':
+        # Remember for next time
+        # -----------
+
+        if mode == 'start':
             state = True
+
         elif mode == 'stop':
             state = False
-        #
-        # if mode:
-        #     # In case it's already recording, start over.
-        #     lx.eval('macro.record false')
-        #
-        #     # Start recording.
-        #     lx.eval('macro.record true')
-        #
-        # elif not mode:
-        #     try:
-        #         # Store the temp file in the root kit directory.
-        #         temp_file_path = lx.eval('query platformservice alias ? "kit_mecco_replay:tmp.LXM"')
-        #
-        #         # Try saving the file.
-        #         lx.eval('macro.saveRecorded {%s}' % temp_file_path)
-        #
-        #         # Open the saved macro for editing.
-        #         lx.eval('replay.fileOpen {%s}' % temp_file_path)
-        #
-        #     except:
-        #         # The Macro recording was probably empty.
-        #         pass
 
-    def cmd_Query(self,index,vaQuery):
-        # Fires whenever any argument is queried.
+        else:
+            state = False if self._recording else True
 
-        va = lx.object.ValueArray()
-        va.set(vaQuery)
+        self.set_state(state)
 
-        # args = self.commander_arguments()
-        #
-        # if index < len(args):
-        #     is_query = 'query' in args[index].get(FLAGS, [])
-        #     is_not_fcl = args[index].get(VALUES_LIST_TYPE) != FCL
-        #     has_recent_value = self._commander_stored_values[index]
-        #
-        #     if is_query and is_not_fcl and has_recent_value:
-        #         va.AddString(has_recent_value)
+        # Do the work
+        # -----------
 
-        lx.out('queried', int(self._recording))
+        if state:
+            # In case it's already recording, start over.
+            lx.eval('macro.record false')
 
-        va.AddInt(int(self._recording))
+            # Start recording.
+            lx.eval('macro.record true')
 
-        return lx.result.OK
+        elif not state:
+            try:
+                # Store the temp file in the root kit directory.
+                temp_file_path = lx.eval('query platformservice alias ? "kit_mecco_replay:tmp.LXM"')
+
+                # Try saving the file.
+                lx.eval('!!macro.saveRecorded {%s}' % temp_file_path)
+
+                # Open the saved macro for editing.
+                lx.eval('!!replay.fileInsert {%s}' % temp_file_path)
+
+            except:
+                # The Macro recording was probably empty.
+                raise Exception("Unable to save and reload temp file. Abort.")
+
+
+    def commander_query(self, arg_index):
+        if arg_index  == 1:
+            return self._recording
 
 
 lx.bless(RecordCommandClass, 'replay.record')
