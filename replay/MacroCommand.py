ed698e5abf51a43283db00988562ae803e472f6f
ArmanHunanyan <arman.hunanyan@gmail.com>
2017-03-07 22:09:06 +0400

    small refactoring
    small refactoring

--- replay/MacroCommand.py
+++ replay/MacroCommand.py
@@ -221,12 +221,7 @@ class MacroCommand(lumberjack.TreeNode):
         for arg in self.args:
             json_arg = next((x for x in json_args if x['argName'] == arg.argName))
             if json_arg is not None:
-                if arg.argType == 1:
-                    arg.value = None if json_arg['value'] is None else int(json_arg['value'])
-                elif arg.argType == 2:
-                    arg.value = None if json_arg['value'] is None else float(json_arg['value'])
-                else:
-                    arg.value = None if json_arg['value'] is None else str(json_arg['value'])
+                arg.value = None if json_arg['value'] is None else self.convert_string_to_value(arg.argType, json_arg['value'])
 
     def get_next_arg_name(self, args_string):
 
@@ -287,6 +282,16 @@ class MacroCommand(lumberjack.TreeNode):
 
         return result, args_string_left
 
+    def convert_string_to_value(self, tp, arg_value):
+        if tp == 1 and isinstance[arg_value, basestring]:
+            return True if arg_value.lower() in ['true', 'on', 'yes'] else False
+        elif tp == 1 and isinstance[arg_value, (bool, int)]:
+            return int(arg_value)
+        elif tp == 2:
+            return float(arg_value)
+        else:
+            return str(arg_value)
+
     def parse_args(self, args_string):
         """Parse a string containing arguments and stores them in 'args'."""
 
@@ -313,14 +318,7 @@ class MacroCommand(lumberjack.TreeNode):
                 arg_number = arg_counter
 
             # Set the value of the argument:
-            if self.args[arg_number].argType == 1 and isinstance[arg_value, basestring]:
-                self.args[arg_number].value = True if arg_value.lower() in ['true', 'on', 'yes'] else False
-            elif self.args[arg_number].argType == 1 and isinstance[arg_value, (bool, int)]:
-                self.args[arg_number].value = int(arg_value)
-            elif self.args[arg_number].argType == 2:
-                self.args[arg_number].value = float(arg_value)
-            else:
-                self.args[arg_number].value = str(arg_value)
+            self.args[arg_number].value = self.convert_string_to_value(self.args[arg_number].argType, arg_value)
 
             # Increase the argument counter, and check if it's not out of bounds:
             if arg_counter == len(self.args): raise Exception("Error in parsing: too many arguments detected.")
