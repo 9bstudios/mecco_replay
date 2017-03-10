d0e83f7e4deb08958d24f042c4b7a825dae87f91
adamohern <adam@mechanicalcolor.com>
2017-03-10 08:15:34 -0500

    added app.quit listener

---
+++ lxserv/replay_appQuit.py
@@ -0,0 +1,34 @@
+# python
+
+import lxifc, modo, lx
+svc_listen = lx.service.Listener()
+
+class SimpleCmdListener(lxifc.CmdSysListener):
+    def __init__(self):
+        svc_listen.AddListener(self)
+        self.armed = True
+
+    def cmdsysevent_ExecutePre(self,cmd,type,isSandboxed,isPostCmd):
+        if self.armed:
+            cmd = lx.object.Command(cmd)
+            # lx.out("'%s' will fire shortly" % cmd.Name())
+            if cmd.Name() == "app.quit":
+                lx.eval('replay.fileClose')
+
+    def cmdsysevent_ExecutePost(self,cmd,isSandboxed,isPostCmd):
+        # if self.armed:
+        #     cmd = lx.object.Command(cmd)
+        #     lx.out("'%s' has finished" % cmd.Name())
+        pass
+
+    def cmdsysevent_RefireBegin(self):
+        # we don't want a bunch of events when the user is
+        # dragging a minislider or something like that,
+        # so we disarm the listener on RefireBegin...
+        self.armed = False
+
+    def cmdsysevent_RefireEnd(self):
+        # ... and rearm on RefireEnd
+        self.armed = True
+
+cmdListener1 = SimpleCmdListener()
