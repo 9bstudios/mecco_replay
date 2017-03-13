db22cbeb9728c435c4cb23ef26fc27ccff9e576e
adamohern <adam@mechanicalcolor.com>
2017-03-13 11:14:50 -0400

    implemented mapToKey

--- lxserv/replay_fileOpenExternal.py
+++ lxserv/replay_fileOpenExternal.py
@@ -22,8 +22,6 @@ class CommandClass(replay.commander.CommanderClass):
     def basic_Enable(self, msg):
         if lx.eval('replay.record query:?'):
             return False
-        if not replay.Macro().file_path:
-            return False
         return True
 
 lx.bless(CommandClass, 'replay.fileOpenExternal')
