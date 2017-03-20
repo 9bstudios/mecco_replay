a9dcc71125341708389a6d60ebd79e16e9305251
adamohern <adam@mechanicalcolor.com>
2017-03-20 19:24:47 -0400

    updated autoTest

--- Scripts/replay_autoTest.py
+++ Scripts/replay_autoTest.py
@@ -2,6 +2,8 @@
 
 import lx, modo, replay, traceback, os, sys, filecmp
 
+RED = '\x03(c:33488896)'
+
 kitpath = lx.eval('query platformservice alias ? "kit_mecco_replay:"')
 
 # open event log
@@ -21,44 +23,50 @@ test_file = 'Test01.LXM'
 
 # Open a macro
 try:
-    lx.eval('replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
+    lx.eval('!!replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
     lx.out("Opened %s" % test_file)
 except:
+    lx.out(RED + "fileOpen failed: %s" % filename)
     traceback.print_exc()
 
 # Close a macro
 try:
-    lx.eval('replay.fileClose')
+    lx.eval('!!replay.fileClose')
     lx.out("Closed %s" % test_file)
 except:
+    lx.out(RED + "fileClose failed: %s" % filename)
     traceback.print_exc()
 
 # Reopen macro
 try:
-    lx.eval('replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
+    lx.eval('!!replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
     lx.out("Reopened %s" % test_file)
 except:
+    lx.out(RED + "fileOpen failed: %s" % filename)
     traceback.print_exc()
 
 # New Macro
 try:
-    lx.eval('replay.fileNew')
+    lx.eval('!!replay.fileNew')
     lx.out("Created new while %s was open." % test_file)
 except:
+    lx.out(RED + "fileNew failed: %s" % filename)
     traceback.print_exc()
 
 # Reopen macro
 try:
-    lx.eval('replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
+    lx.eval('!!replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
     lx.out("Reopened %s" % test_file)
 except:
+    lx.out(RED + "fileOpen failed: %s" % filename)
     traceback.print_exc()
 
 # Insert macro steps
 try:
-    lx.eval('replay.fileInsert {%s}' % os.path.join(kitpath, 'utest', test_file))
+    lx.eval('!!replay.fileInsert {%s}' % os.path.join(kitpath, 'utest', test_file))
     lx.out("Inserted %s" % test_file)
 except:
+    lx.out(RED + "fileInsert failed: %s" % filename)
     traceback.print_exc()
 
 # -----------------
@@ -100,46 +108,41 @@ for test_file in test_files:
 
     # Open a macro
     try:
-        lx.eval('replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
+        lx.eval('!!replay.fileOpen {%s}' % os.path.join(kitpath, 'utest', test_file))
         lx.out("Opened %s" % test_file)
     except:
+        lx.out(RED + "fileOpen failed: %s" % filename)
         traceback.print_exc()
 
     # Play a macro
     try:
-        lx.eval('replay.play')
+        lx.eval('!!replay.play')
         lx.out("Played %s" % test_file)
     except:
+        lx.out(RED + "play failed: %s" % filename)
         traceback.print_exc()
 
     lx.eval('!!scene.closeAll')
 
-    # Step through an entire macro
-    for n in range(len(replay.Macro().commands)):
-        try:
-            lx.eval('replay.step')
-            lx.out("Ran %s step %s" % (test_file, n))
-        except:
-            lx.out("Failed %s step %s" % (test_file, n))
-            traceback.print_exc()
-
-    # Save a macro
-    try:
-        lx.eval('replay.fileSave {%s}' % os.path.join(file_path, name + '_save.lxm'))
-        lx.out("Saved %s" % name + '_save.lxm')
-    except:
-        lx.out("Save failed: %s" % name + '_save.lxm')
-        traceback.print_exc()
+    # TODO: REQUIRES LINE SELECTION
+    # # Step through an entire macro
+    # for n in range(len(replay.Macro().commands)):
+    #     try:
+    #         lx.eval('!!replay.step')
+    #         lx.out("Ran %s step %s" % (test_file, n))
+    #     except:
+    #         lx.out("Failed %s step %s" % (test_file, n))
+    #         traceback.print_exc()
 
     for export_format in ['lxm', 'py', 'json']:
         filename = name + '_export.' + export_format
 
         # Export macro
         try:
-            lx.eval('replay.fileExport %s {%s}' % (export_format, os.path.join(file_path, filename)))
+            lx.eval('!!replay.fileExport %s {%s}' % (export_format, os.path.join(file_path, filename)))
             lx.out("Exported %s" % filename)
         except:
-            lx.out("Export failed: %s" % filename)
+            lx.out(RED + "Export failed: %s" % filename)
             traceback.print_exc()
 
     for export_format in ['lxm', 'py', 'json']:
@@ -150,33 +153,33 @@ for test_file in test_files:
 
         # Reopen exported macro
         try:
-            lx.eval('replay.fileOpen {%s}' % test_file_fullpath)
+            lx.eval('!!replay.fileOpen {%s}' % test_file_fullpath)
             lx.out("Opened %s" % filename)
         except:
-            lx.out("Failed to re-open %s" % filename)
+            lx.out(RED + "Failed to re-open %s" % filename)
             traceback.print_exc()
 
         # Re-Export macro for comparison with original
         try:
-            lx.eval('replay.fileExport lxm {%s}' % comparison_file_fullpath)
+            lx.eval('!!replay.fileExport lxm {%s}' % comparison_file_fullpath)
             lx.out("Exported %s" % comparison_filename)
         except:
-            lx.out("Export failed: %s" % comparison_filename)
+            lx.out(RED + "Export failed: %s" % comparison_filename)
             traceback.print_exc()
 
         # Compare re-exported file with original
         try:
             if filecmp.cmp(test_file_fullpath, comparison_file_fullpath):
-                lx.out("%s and %s match. Pass." % (filename, comparison_filename))
+                lx.out(RED + "%s and %s match. Pass." % (filename, comparison_filename))
             else:
-                lx.out("%s and %s do not match. FAIL." % (filename, comparison_filename))
+                lx.out(RED + "%s and %s do not match. FAIL." % (filename, comparison_filename))
         except:
-            lx.out("Failed to compare %s with %s" % (filename, comparison_filename))
+            lx.out(RED + "Failed to compare %s with %s" % (filename, comparison_filename))
             traceback.print_exc()
 
     # Close a macro
     try:
-        lx.eval('replay.fileClose')
+        lx.eval('!!replay.fileClose')
         lx.out("Closed %s" % test_file)
     except:
         traceback.print_exc()
