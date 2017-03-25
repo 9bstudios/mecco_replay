47449c5f1c5916a68bacb434b3595cc07b3e5edc
ArmanHunanyan <arman.hunanyan@gmail.com>
2017-03-25 20:24:16 +0400

    Fixed #78 lineDelete does not work for blocked commands
    Fixed #78 lineDelete does not work for blocked commands

--- lxserv/replay_lineDelete.py
+++ lxserv/replay_lineDelete.py
@@ -8,15 +8,15 @@ class CommandClass(replay.commander.CommanderClass):
     """Deletes the currently-selected command from the `Macro()` object."""
     def commander_execute(self, msg, flags):
 
-        # collect list of selected indices
-        indices = list()
+        # collect list of selected paths
+        paths = list()
         for line in replay.Macro().selected_descendants:
-            indices.append(line.index)
+            paths.append(line.path)
 
         # Register Undo object performing operation and apply it
         undo_svc = lx.service.Undo()
         if undo_svc.State() != lx.symbol.iUNDO_INVALID:
-            undo_svc.Apply(UndoLineDelete(indices))
+            undo_svc.Apply(UndoLineDelete(paths))
 
     def basic_Enable(self, msg):
         if lx.eval('replay.record query:?'):
@@ -24,10 +24,11 @@ class CommandClass(replay.commander.CommanderClass):
         return bool(replay.Macro().selected_descendants)
 
 class UndoLineDelete(lxifc.Undo):
-    def __init__(self, indices):
-        self.m_indices = indices
-        # Need to delete indices sorted in reverse order to not invalidate other indices
-        self.m_indices.sort(reverse=True)
+    def __init__(self, paths):
+        self.m_paths = paths
+        # Need to delete paths sorted in reverse order to not invalidate other paths
+        # Default list comparision working in our case
+        self.m_paths.sort(reverse=True)
         self.m_deleted_commands = list()
 
     def finalize_command(self, macro):
@@ -42,10 +43,10 @@ class UndoLineDelete(lxifc.Undo):
         macro = replay.Macro()
         del self.m_deleted_commands[:]
 
-        # delete selected indices and store them in json form to be able to undo
-        for index in self.m_indices:
-            child = macro.children[index]
-            self.m_deleted_commands.append((index, child.render_json()))
+        # delete selected paths and store them in json form to be able to undo
+        for path in self.m_paths:
+            child = macro.node_for_path(path)
+            self.m_deleted_commands.append((path, child.render_json()))
             child.delete()
 
         self.finalize_command(macro)
@@ -53,13 +54,14 @@ class UndoLineDelete(lxifc.Undo):
     def undo_Reverse(self):
         macro = replay.Macro()
 
-        # Sort indices in ascending order for undo operation
+        # Sort paths in ascending order for undo operation
+        # Default list comparision working in our case
         self.m_deleted_commands.sort(key=lambda x: x[0])
 
         # Restore deleted commands
-        for removed_index, json in self.m_deleted_commands:
-            macro.add_command(command_json = json, index = removed_index)
-            macro.children[removed_index].selected = True
+        for removed_path, json in self.m_deleted_commands:
+            macro.add_command(command_json = json, path = removed_path)
+            macro.node_for_path(removed_path).selected = True
 
         self.finalize_command(macro)
 
