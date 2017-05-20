1416febb6f94cf237650db0663076a9a5c34099e
ArmanHunanyan <arman.hunanyan@gmail.com>
2017-05-20 22:01:21 +0400

    Fixed 166
    Fixed 166

---
+++ lxserv/replay_preferance.py
@@ -0,0 +1,51 @@
+# python
+
+import lx, lxifc, modo, replay
+
+class CommandClass(replay.commander.CommanderClass):
+    def commander_arguments(self):
+        return [
+                {
+                    'name': 'value',
+                    'datatype': 'string',
+                    'default': "",
+                    'flags': ['variable', 'query'],
+                }, {
+                    'name': 'name',
+                    'datatype': 'string',
+                    'flags': ['reqforvariable'],
+                }
+            ]
+
+    def commander_execute(self, msg, flags):
+        value = self.commander_arg_value(0)
+        name = self.commander_arg_value(1)
+
+        lx.eval("user.value %s %s" % (name, value))
+
+        notifier = replay.Notifier()
+        notifier.Notify(lx.symbol.fCMDNOTIFY_CHANGE_ALL)
+        
+    def basic_Enable(self, msg):
+        if lx.eval('replay.record query:?'):
+            return False
+        return True
+
+    def cmd_Query(self, index, vaQuery):
+        name = self.commander_arg_value(1)
+
+        # Create the ValueArray object
+        va = lx.object.ValueArray()
+        va.set(vaQuery)
+
+        va.AddInt(lx.eval("user.value %s ?" % name))
+        
+        return lx.result.OK
+        
+    def basic_ArgType(self, argIndex):
+        return 'boolean'
+
+    def commander_notifiers(self):
+        return [("replay.notifier", "")]
+
+lx.bless(CommandClass, "replay.preferance")
